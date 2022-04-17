43c2e776b22ea81ddee398e563181c0c3c033723
Your Name <you@example.com>
2022-04-17 16:50:50 +0200

    Removed some unused entries from test_grpc_server

--- ml_api/test_grpc_server.py
+++ ml_api/test_grpc_server.py
@@ -1,14 +1,12 @@
 import argparse
 import grpc
-from lib.grpc.spaghetticheck_pb2 import Image, ObjectHit, AnalyzedFrame, Placement
+from lib.grpc.spaghetticheck_pb2 import Image
 from lib.grpc.spaghetticheck_pb2_grpc import ImageAnalyzeStub
 
 def parse_args():
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--grpc-server", default="localhost:50051")
     parser.add_argument("image", help="Image to test w ith")
-
     return parser.parse_args()
 
 args =  parse_args()
